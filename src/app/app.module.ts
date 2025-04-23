16278782a1e4c8fcc4a4c1c9c29612caab0a67bd
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2025-04-23 12:03:29 -0500

    Add auth and product components
    Created the Login and Register components for user authentication, as well as ProductList and ProductForm components to manage product display and input.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductFormComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
